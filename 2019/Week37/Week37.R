f7b6f9ed1f4120256b9e64a72ef0a4ff3b7df5a2
Anish Joni <janishjoni@gmail.com>
2023-07-17 12:12:01 -0400

    Cleaning repo

--- 2019/Week37/Week37.R
+++ 2019/Week37/Week37.R
@@ -10,24 +10,25 @@ library(magrittr)
 # Data --------------------------------------------------------------------
 set.seed(20181209)
 
-df <- read_csv("https://data.cityofnewyork.us/api/views/43nn-pn8j/rows.csv")
+#df <- read_csv("https://data.cityofnewyork.us/api/views/43nn-pn8j/rows.csv")
+setwd("~/GitHub/Tidy Tuesday/Week37")
+NYC_df <- read_csv("nyc_restaurants.csv")
 
 # Cleaning names with janitor, sampling 300,000 records, and dropping some variables
-NYC_df <- df %>% 
-  janitor::clean_names() %>%
-  select(-phone, -grade_date, -record_date, -building, -street) %>% 
-  sample_n(size = 300000)
+# NYC_df %<>%
+#   janitor::clean_names() %>%
+#   select(-phone, -grade_date, -record_date, -building, -street) %>%
+#   sample_n(size = 300000)
 
 # save the .csv
-write_csv(NYC_df, "nyc_restaurants.csv")
+# write_csv(NYC_df, "nyc_restaurants.csv")
 
 # Extracting Geo data for regions in NY state
-zipcode %<>% 
-  filter(state == "NY")
+data("zipcode")
 
-zipcode_df <- tbl_df(zipcode)
-
-zipcode_df %<>% 
+zipcode_df <- zipcode %>% 
+  tbl_df() %>% 
+  filter(state == "NY") %>% 
   mutate(zip = as.integer(zip))
 
 # EDA ---------------------------------------------------------------------
